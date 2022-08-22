bb9ffeb78ce9a8e38029641f975419991c990bf7
Paulo Almeida <paulo.vitor102@gmail.com>
2022-08-22 13:52:07 +0000

    updated static pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path('about', view=views.about, name='about'),
     # path for contact us view
-
+    path('contact', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
